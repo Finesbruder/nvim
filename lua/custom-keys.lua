fbdda85fa63e8b101a02747e384e49a927aa7b06
= <=>
2024-01-14 14:56:52 +0100

    Add autocompletion.

--- lua/custom-keys.lua
+++ lua/custom-keys.lua
@@ -5,9 +5,11 @@ vim.keymap.set("i", "jk", "<ESC>", {})
 vim.keymap.set("i", "kj", "<ESC>", {})
 
 -- Navigation
-vim.keymap.set("n", "<S-tab>", ":bprev<CR>", {})
 vim.keymap.set("n", "<tab>", ":bnext<CR>", {})
-vim.keymap.set("n", "<leader>g", ":Neotree focus git_status<CR>", {})
+vim.keymap.set("n", "<S-tab>", ":bprev<CR>", {})
+vim.keymap.set("n", "<C-k>", "<C-w>w", {})
+vim.keymap.set("n", "<C-j>", "<C-w>W", {})
+vim.keymap.set("n", "<leader>g", ":Neotree reveal git_status<CR>", {})
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
     -- center cursor 
@@ -24,28 +26,31 @@ vim.keymap.set("n", "<leader>d", ":bp<bar>sp<bar>bn<bar>bd!<CR>", {});
 -- Code Insights
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 -- vim.keymap.set("n", "<leader>gd", "gd | :tabnew %<CR>", {}) funktioniert noch nicht ganz: öffnet nicht die definition
-vim.keymap.set("n", "<C-j>", vim.lsp.buf.hover, {})
-vim.keymap.set("i", "<C-j>", vim.lsp.buf.hover, {})
+-- vim.keymap.set("n", "<C-j>", vim.lsp.buf.hover, {})
+-- vim.keymap.set("i", "<C-j>", vim.lsp.buf.hover, {})
 
--- Code Actions
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+-- LSP Bindings
+vim.keymap.set("n", "<leader>ra", vim.lsp.buf.code_action, {})
 vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, {}) -- Brauche noch global rename
+vim.keymap.set("n", '<leader>rr', vim.lsp.buf.references, {})
+vim.keymap.set("n", '<leader>rws', vim.lsp.buf.workspace_symbol, {}) -- ich weiß nicht was das macht lol
+vim.keymap.set("i", '<C-h>', vim.lsp.buf.signature_help, {}) -- ich weiß nicht was das macht lol
 
 -- Save file
 vim.keymap.set('n', '<C-s>', ":w<CR>", {})
-vim.keymap.set('i', '<C-s>', "<ESC>:w<CR>", {})
+vim.keymap.set('i', '<C-s>', "<ESC>:w<CR>i", {})
 vim.keymap.set('n', '<leader><C-s>', ":saveas %:h\\", {})
 vim.keymap.set('i', '<leader><C-s>', ":saveas %:h\\", {})
 
 -- Refactoring
-vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, {}) -- Brauche noch global rename
 vim.keymap.set("v", '<C-s>', ":sort<CR>", {}) -- Brauche noch global rename
 vim.keymap.set("n", "<A-down>", ":move +1<CR>", {})
 vim.keymap.set("n", "<A-up>", ":move -2<CR>", {})
 vim.keymap.set("v", "<leader>r", "\"hy:%s/<C-r>h//g<left><left>", {})
 
     -- Deletes selected text, moves to new window, pastes and saves in cwd 
-vim.keymap.set("v", "<tab>", "d<bar>:vsplit<CR><bar>:e %:h\\tempfile.txt<CR><bar>p<bar>:saveas %:h\\", {}) 
+vim.keymap.set("v", "<tab>", "d<bar>:vsplit<CR><bar>:e %:h\\tempfile.txt<CR><bar>p<bar>:saveas %:h\\", {})
 
 -- Code Hilfe 
     -- Brackets
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,23 +1,38 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end
     },{
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
-            })
-        end
+        lazy = false,
+        opts = {
+            auto_install = true,
+          },
+        -- config = function()
+        --     require("mason-lspconfig").setup({
+        --         ensure_installed = { "lua_ls", "tsserver" }
+        --     })
+        -- end
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
+            lspconfig.tsserver.setup({
+              capabilities = capabilities
+            })
+            lspconfig.html.setup({
+              capabilities = capabilities
+            })
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities
+            })
         end
     }
 }
